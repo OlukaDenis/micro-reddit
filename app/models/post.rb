22dd7fcb2872f3a023bc2b911e6518bc5a5f5dc4
OlukaDenis <olukadeno@gmail.com>
2020-01-10 00:37:03 +0300

    Fixed linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   validates :title, presence: true,
-                      length: { minimum: 10 }
+                    length: { minimum: 10 }
   validates :content, presence: true,
-                   length: { minimum: 10 }
+                      length: { minimum: 10 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   validates :username, presence: true,
-                      length: { in: 3..10 },
-                      uniqueness: true
+                       length: { in: 3..10 },
+                       uniqueness: true
 end
